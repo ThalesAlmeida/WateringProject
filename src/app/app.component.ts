77f706d369838f9848a80ec9ad185533540ff26e
Thales <thalesalves44@gmail.com>
2018-11-03 10:28:51 -0300

    Adicionando sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,28 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
+import { ContactPage } from '../pages/contact/contact';
+import { PropriedadePage } from '../pages/propriedade/propriedade';
+import { CultivoPage } from '../pages/cultivo/cultivo';
+import { AcompanharIrrigacaoPage } from '../pages/acompanhar-irrigacao/acompanhar-irrigacao';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  home:any = HomePage;
+  propriedade:any = PropriedadePage; 
+  cultivo:any = CultivoPage;
+  acompanharIrrigacao:any = AcompanharIrrigacaoPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      
     });
+  }
+
+  public openPage(opcao){
+    this.rootPage = opcao
   }
 }
