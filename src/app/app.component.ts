427cfcaf198d9bbb067dd6f93ad16cd69eff2d3f
Thales <thalesalves44@gmail.com>
2018-11-11 12:47:51 -0300

    Construindo página de acompanhamento da irrigação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,10 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
-import { HomePage } from '../pages/home/home';
-import { ContactPage } from '../pages/contact/contact';
 import { PropriedadePage } from '../pages/propriedade/propriedade';
 import { CultivoPage } from '../pages/cultivo/cultivo';
 import { AcompanharIrrigacaoPage } from '../pages/acompanhar-irrigacao/acompanhar-irrigacao';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
